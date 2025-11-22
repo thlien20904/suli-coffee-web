9c0468d6b173f19a73cf963d40761052dec95aa1
thlien20904 <thuylien2k4@gmail.com>
2025-11-23 00:18:37 +0700

    update

--- src/pages/user/successful.js
+++ src/pages/user/successful.js
@@ -92,7 +92,10 @@ export default function Successful() {
           <FontAwesomeIcon icon={faExclamationTriangle} className="me-2" />
           {error}
           <div className="mt-3">
-            <Button variant="primary" onClick={() => navigate("/profile/orders")}>
+            <Button
+              variant="primary"
+              onClick={() => navigate("/profile/orders")}
+            >
               Xem đơn hàng
             </Button>
             <Button
@@ -240,7 +243,7 @@ export default function Successful() {
           </Button>
           <Button
             variant="outline-primary"
-            onClick={() => navigate("/profile")}
+            onClick={() => navigate("/profile/orders")}
           >
             Xem đơn hàng
           </Button>
